b7adba7aace2fbafe23076cffb8c5e6b82f7c684
JestemBartek <maciek.bartynski@gmail.com>
2018-11-23 19:46:10 +0100

    navi

--- src/scripts/sections-slider/sections-slider.js
+++ src/scripts/sections-slider/sections-slider.js
@@ -2,102 +2,94 @@ import jquery from 'jquery';
 
 class BlobMovements {
     constructor(){
-        this.blob = jquery('#sections-blob');
-        this.slider = jquery('#sections-slider');
-        this.container = jquery('#sections--wrapper');
-        this.naviItems = document.querySelectorAll('.navigation--item');
-        this.scrollingState = document.querySelectorAll('.scrolling-state');
-        this.currentSectionContainer = document.querySelector('#current-section-container');
-        this.currentlyDispalyedNum=null;
+        this.sectionsWrapper = jquery('.sections-wrapper');
+        this.sectionsItem = jquery('.sections-wrapper_item');
+        this.sectionsContent = jquery('.sections-wrapper_content');
+        this.naviItems = jquery('.navigation--item');
+        this.navi = jquery('.navigation--container');
 
-        this.amountOfCells;
-        this.sliderStep;
-        this.setScrollingStages();
+        this.isMouseOver=false;
+               
+        //this.blob = jquery('#sections-blob');
+        //this.container = jquery('#sections--wrapper');
+        //this.naviItems = document.querySelectorAll('.navigation--item');
+        //this.scrollingState = document.querySelectorAll('.scrolling-state');
+        //this.currentSectionContainer = document.querySelector('#current-section-container');
+        //this.currentlyDispalyedNum=null;
+
+        //this.amountOfCells;
+        //this.sliderStep;
+        //this.setScrollingStages();
         
-        this.timer;
-        this.containerHidden=true;
-        this.onMouseOver();
+        //this.timer;
+        //this.containerHidden=true;
+        //this.onMouseOver();
 
         //ini:
-        this.showTheSec(0);
+        //this.showTheSec(0);
+        this.showBlobOnMouseOverNaviItem = this.showBlobOnMouseOverNaviItem.bind(this);
+        this.hideBlobOnMouseOutNaviItem = this.hideBlobOnMouseOutNaviItem.bind(this);
+        this.checkIfMouseIsOverNav();
+        this.mouseOverDispatcher();
     }
 
-    setScrollingStages(){
-        let amountOfCells = this.scrollingState.length-1;
-        let stateWidth = 100/amountOfCells;
-        let sliderStep = (stateWidth * 100 / 20);
-
-        this.slider.css('grid-template-columns', `repeat(${amountOfCells + ', ' + stateWidth }%`);
-        this.sliderStep=sliderStep;
-        this.amountOfCells = amountOfCells;
+    //events
+    mouseOverDispatcher(){
+        let dispatchingContext = this.showBlobOnMouseOverNaviItem;
+        let dispatchingContextB = this.hideBlobOnMouseOutNaviItem;
+        this.naviItems.each(
+            function (index) {
+                jquery(this).hover(()=>{
+                    dispatchingContext(index);
+                },()=>{
+                    dispatchingContextB(index);
+                });
+            }
+        );
     }
 
-    onMouseOver(){
-        for (let i=0; i<=this.amountOfCells;i++){
-
-            this.naviItems[i].addEventListener('click', () => {
-                this.showTheSec(i);
-            })
-            
-            this.naviItems[i].addEventListener('mouseover', ()=>{
-                this.moveTheBlob(i);
-            })
-            
-            this.naviItems[i].addEventListener('mouseout', ()=>{
-                this.timer=setTimeout(()=>{
-                    this.container.fadeOut('slow');
-                    this.containerHidden=true;
-                }, 1000);
-            })
-
-        }
+    showBlobOnMouseOverNaviItem(num){
+        //if is mouse over, bigger all
+        let isMouseHere = this.isMouseOver;
+        this.sectionsItem.each(
+            function(idx){  
+                let currentItem = jquery(this);
+                if(idx===num){
+                    currentItem.addClass('sections-wrapper_item--shadow');
+                    currentItem.find('.sections-wrapper_content').fadeIn(100);
+                }
+                if(isMouseHere===false){
+                    currentItem.fadeIn(1);
+                    let top = jquery(this).position().top;
+                    let left = jquery(this).position().left;
+                    currentItem.find('.sections-wrapper_content').css('top', -top + 'px');
+                    currentItem.find('.sections-wrapper_content').css('left', -left + 'px');
+                    currentItem.addClass('sections-wrapper_item--bigger');
+                }
+            }
+        )
+        this.isMouseOver=true;
     }
 
-    moveTheBlob(num){
-        
-        if (num > this.currentlyDispalyedNum && this.currentlyDispalyedNum!==null) {
-            num -= 1;
-        } else if (num === this.currentlyDispalyedNum) {
-            return;
-        }
-
-        clearTimeout(this.timer);
-
-        if (this.containerHidden) {
-            this.container.fadeIn();
-            this.containerHidden = false;
-        }
-
-        //let x=`${num*(75/this.amountOfCells)}%`;
-        var z = `${num}vw`;
-        if (num >= this.amountOfCells / 2){
-            z = `${this.amountOfCells - num}vw`
-        }
-        let x = `${num}vw`;
-        let y=`${num*-this.sliderStep}%`;
-        this.blob.css('left', z);
-        this.container.css('top', x);
-        this.slider.css('left', y);
+    hideBlobOnMouseOutNaviItem(num){
+        this.sectionsItem.each(function (index) {
+            let currentItem = jquery(this);
+            index===num?currentItem.find('.sections-wrapper_content').css('display', 'none'):null;
+            currentItem.removeClass('sections-wrapper_item--shadow');
+        });
     }
 
-    showTheSec(num){
-        
-        clearTimeout(this.timer);
-        this.container.fadeOut('200');
-        this.containerHidden = true;
-     
-        let currentlyDisplayed = this.currentSectionContainer.querySelector('.section-content--wrapper');
-        if (currentlyDisplayed) {
-            this.scrollingState[this.currentlyDispalyedNum].style.display='block';
-            this.scrollingState[this.currentlyDispalyedNum].appendChild(currentlyDisplayed);
-            this.naviItems[this.currentlyDispalyedNum].classList.remove('active');
-        }
-        
-        let clickedSec = this.scrollingState[num].querySelector('.section-content--wrapper');
-        this.scrollingState[num].style.display='none';
-        this.currentSectionContainer.appendChild(clickedSec);
-        this.naviItems[num].classList.add('active');
-        this.currentlyDispalyedNum=num;
+    checkIfMouseIsOverNav (){
+        this.navi.mouseleave(()=>{
+            this.isMouseOver=false;
+            console.log('over')
+            this.sectionsItem.each(
+                function(index){
+                    jquery(this).fadeOut(100);
+                    jquery(this).removeClass('sections-wrapper_item--bigger');
+                }
+            );
+        });
     }
 }
 
